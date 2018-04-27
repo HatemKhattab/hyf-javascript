6c5d4b5c50ba5fe83ea604240d3c83be83544ee8
Hatem Khattab <hattemkhattab@gmail.com>
2018-04-27 21:43:30 +0200

    add html and js

---
+++ week4/script.js
@@ -0,0 +1,16 @@
+var students = [
+  {
+    id: "1",
+    name: "David",
+    education: "Art",
+    city: "Växjö",
+    pic: ""
+  },
+  {
+    id: "2",
+    name: "Per",
+    education: "Economy",
+    city: "Götenborg",
+    pic: ""
+  }
+];
