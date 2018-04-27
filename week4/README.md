0c1f083aed7507d2015e539bd674a92d9d66cea7
Hatem Khattab <hattemkhattab@gmail.com>
2018-04-28 00:59:59 +0200

    Add more students

---
+++ week4/README.md
@@ -0,0 +1,2 @@
+## Week 4 Assignment
+Create a dynamic student table with 5 columns (Student ID, Student Name, Education, City, Student Picture) using javaScript
--- week4/script.js
+++ week4/script.js
@@ -4,7 +4,7 @@ var students = [
     name: "David",
     education: "Art",
     city: "Växjö",
-    pic: ""
+    pic: "./images/david.jpg"
   },
 
   {
@@ -12,6 +12,45 @@ var students = [
     name: "Per",
     education: "Business",
     city: "Götenborg",
-    pic: ""
+    pic: "./images/per.jpg"
+  },
+
+  {
+    id: "3",
+    name: "Alex",
+    education: "IT",
+    city: "Stockholm",
+    pic: "./images/alex.jpg"
+  },
+
+  {
+    id: "4",
+    name: "Annica",
+    education: "Home Desig",
+    city: "Uppsala",
+    pic: "./images/annica.jpg"
   }
+
+  
 ];
+
+function createStudents(arr) {
+  var table= document.createElement('table');
+  var header = document.createElement('tr')
+  //write the header row
+  for(var key in arr[0]){
+    console.log(key)
+    header.innerHTML += `<th>${key.toUpperCase()}</th>`;
+  }
+  table.appendChild(header);
+  for(let i=0; i<arr.length; i++){
+    var row = document.createElement('tr');
+    row.innerHTML = `<td>${arr[i].id}</td>
+                     <td>${arr[i].name}</td>
+                     <td>${arr[i].education}</td>
+                     <td>${arr[i].city}</td>
+                     <td><img src=${arr[i].pic}></td>`;
+    table.appendChild(row);
+  }
+  document.body.appendChild(table)
+}
