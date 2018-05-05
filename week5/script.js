27ee97f65c08280db47ba4a899a074b5dc44f605
Hatem Khattab <hattemkhattab@gmail.com>
2018-05-06 01:25:35 +0200

    remove elemnt with duration less than 2 hours

--- week5/script.js
+++ week5/script.js
@@ -1,4 +1,4 @@
-//2.1
+// 2.1
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function doubleNum(num){
@@ -14,3 +14,73 @@ function doubleOddNumbers(arr) {
 console.log(doubleOddNumbers(numbers));
 
 //2.2
+const monday = [
+  {
+    name: 'Write a summary HTML/CSS',
+    duration: 180
+  },
+  {
+    name: 'Some web development',
+    duration: 120
+  },
+  {
+    name: 'Fix homework for class10',
+    duration: 20
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200
+  }
+];
+
+const tuesday = [
+  {
+    name: 'Keep writing summary',
+    duration: 240
+  },
+  {
+    name: 'Some more web development',
+    duration: 180
+  },
+  {
+    name: 'Staring out the window',
+    duration: 10
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200
+  },
+  {
+    name: 'Look at application assignments new students',
+    duration: 40
+  }
+];
+
+const tasks = monday.concat(tuesday);
+
+function changeToHours(element){
+  element.duration = element.duration/60;
+}
+// change duration to hours
+tasks.map(changeToHours);
+
+// remove element that has duration less than 2 hours
+var tasksModified =  tasks.filter(element => element.duration >= 2);
+
+
+console.log(tasksModified);
+
+//var newNumbers = arr.filter(num => (num%2 !== 0));
+
+//console.log(tasks);
+
+// Write a program that computes how much Maartje has earned by completing these tasks, using map and filter. For the 'summing part' you can try your luck with reduce; alternatively, you may use forEach or a for loop.
+
+// Follow these steps. Each step should build on the result of the previous step.
+
+// Map the tasks to durations in hours.
+// Filter out everything that took less than two hours (i.e., remove from the collection)
+// Multiply the each duration by a per-hour rate for billing (you can decide yourself what Maartje should earn per hour) and sum it all up.
+// Output a formatted Euro amount, rounded to Euro cents, e.g: € 12.34.
+// Choose variable and parameters names that most accurately describe their contents or purpose. When naming an array, use a plural form, e.g. durations. For a single item, use a singular form, e.g. duration. For details, see Naming Conventions.
+// Don't forget to use =>.
