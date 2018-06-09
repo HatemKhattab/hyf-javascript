0c7fa4b69285ec5e0015b1526cf8934f4481c174
Hatem Khattab <hattemkhattab@gmail.com>
2018-06-09 18:28:11 +0200

    change function to return promise

--- try_javascript/app.js
+++ try_javascript/app.js
@@ -1,20 +1,27 @@
-function fetchJSON(url){
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.responseType = 'json';
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) { 
-      if (xhr.status < 400) { 
-        return (xhr.response);
-      } else {
-          console.log('failled') ;
+function fetchJSON(url) {
+  console.log('calling fetch json');
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status < 400) {
+          resolve(xhr.response);
+        } else {
+          reject(new Error(xhr.statusText));
+        }
       }
-    }
-  };
-  xhr.send();
+    };
+    xhr.send();
+  });
 }
 
+const url = 'http://api.nobelprize.org/v1/prize.json?year=2017';
+const value = fetchJSON(url)
+                        .then(data => viewData(data))
+                        .catch(err => renderError(err));
 
-
-let value = fetchJSON('http://api.nobelprize.org/v1/prize.json?year=2017');
-console.log(value);
+function viewData(txt){
+  console.log(txt);
+}
