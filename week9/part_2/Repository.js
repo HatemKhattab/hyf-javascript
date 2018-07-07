c2aec5b02062c2b2ef51135aded002b5df3c2737
Hatem Khattab <hattemkhattab@gmail.com>
2018-07-07 14:32:28 +0200

    Add render function to repository

--- week9/part_2/Repository.js
+++ week9/part_2/Repository.js
@@ -2,4 +2,13 @@ class Repository {
   constructor(data){
     this._data = data;
   }
+
+  render(){
+    const repositoryInfo = document.querySelector('#repo_info');
+    repositoryInfo.innerHTML =``;
+    repositoryInfo.innerHTML =`<strong>Repository  </strong><span>${this._data.name}</span><br>
+                               <strong>Description  </strong><span>${this._data.description}</span><br>
+                               <strong>Forks  </strong><span>${this._data.forks}</span><br>
+                               <strong>Updated  </strong><span>${this._data.updated_at}</span><br>`;
+  }
 }
