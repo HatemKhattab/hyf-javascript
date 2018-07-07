e43800c901d4f78cfdd1319c9234afd6c6961ac9
Hatem Khattab <hattemkhattab@gmail.com>
2018-07-07 14:30:14 +0200

    Add render function

--- week9/part_2/Contributer.js
+++ week9/part_2/Contributer.js
@@ -2,4 +2,12 @@ class Contributer {
   constructor(data){
     this._data = data;
   }
+
+  render(){
+    const repoContributers = document.querySelector('#repo_contributors');
+    repoContributers.innerHTML += `<div class="row">
+                                   <div class="col-xs-6"><h3>${this._data.login}</h3></div>
+                                   <div class="col-xs-6"><img src=${this._data.avatar_url}></div>
+                                   </div>`;
+  }
 }
