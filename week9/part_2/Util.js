1a25f9eb0520a90cc995372725d62fe5916e5d2f
Hatem Khattab <hattemkhattab@gmail.com>
2018-07-07 14:33:22 +0200

    update checkSelectChanging

--- week9/part_2/Util.js
+++ week9/part_2/Util.js
@@ -35,3 +35,34 @@ Util.addSelectElementOptions = function (arr){
     selectElement.appendChild(option);
   });
 }
+
+Util.checkSelectChanging = function(arr) {
+  try {
+    const selectElement = document.getElementById("repositories");
+    selectElement.addEventListener("change", async function(){
+      const selectValue = selectElement.value;
+      const selectedRepo = Util.renderRepositoryInfo(arr, selectValue);
+      const repoContributersUrl = selectedRepo._data.contributors_url;
+      const contributersList = await Util.fetchJSON(repoContributersUrl);
+      Util.renderRepositoryContributers(contributersList);
+    });
+  }catch(err) {
+    console.log(err);
+  }
+}
+
+Util.renderRepositoryInfo = function(arr, value){
+  const repoData = arr.filter(repo => repo.id == value)[0];
+  const repository = new Repository(repoData);
+  repository.render();
+  return repository;
+}
+
+Util.renderRepositoryContributers = function(arr){
+  const repoContributers = document.querySelector('#repo_contributors');
+  repoContributers.innerHTML =``;
+  arr.forEach(function(item){
+  const contributer = new Contributer(item);
+  contributer.render();
+  });
+}
